295834ba3f9754b0a1cca9bd1bd87a3d34e9d567
Estugon <estugon-tem@gmx.de>
2021-12-14 00:56:29 +0100

    Day 13 - small changes

--- day13/day13.cpp
+++ day13/day13.cpp
@@ -6,28 +6,36 @@
 
 using namespace std;
 
-typedef pair<int, int> coord;
+typedef pair<ulong, ulong> coord;
 
 void decodeInstruction(string s, bool *axis, int *pos) {
     *axis = (s[0] == 'x');
     *pos  = stoi(s.substr(2));
 }
 
-vector<coord> fold(bool axis, int at, vector<coord> coords) {
+set<coord> fold(bool axis, int at, set<coord> coords) {
     // axis: True = x, false = y
-    vector<coord> newCoords;
+    set<coord> newCoords;
     for (coord c : coords) {
         if (axis) {
-            int dist = abs(at - c.first);
-            coord newC = {at-dist, c.second};
-            if (newC.first >= 0) {
-                newCoords.push_back(newC);
+            if (c.first > at) {
+                ulong dist = c.first - at;
+                coord newC = {at-dist, c.second};
+                if (newC.first >= 0) {
+                    newCoords.insert(newC);
+                }
+            } else {
+                newCoords.insert(c);
             }
         } else {
-            int dist = abs(at - c.second);
-            coord newC = {c.first, at-dist};
-            if (newC.second >= 0) {
-                newCoords.push_back(newC);
+            if (c.second > at) {
+                ulong dist = c.second - at;
+                coord newC = {c.first, at-dist};
+                if (newC.second >= 0) {
+                    newCoords.insert(newC);
+                }
+            } else {
+                newCoords.insert(c);
             }
         }
     }
@@ -35,9 +43,9 @@ vector<coord> fold(bool axis, int at, vector<coord> coords) {
     return newCoords;
 }
 
-vector<vector<bool>> createPattern(vector<string> instructions, vector<coord> coords) {
+string createPattern(vector<string> *instructions, set<coord> coords) {
     // First evaluate every instruction
-    for (string s : instructions) {
+    for (string s : *instructions) {
         bool axis;
         int  pos;
         decodeInstruction(s, &axis, &pos);
@@ -46,66 +54,47 @@ vector<vector<bool>> createPattern(vector<string> instructions, vector<coord> co
 
     // Now create the field
     // First we need the sizes
-    int cols = 0, rows = 0;
+    ulong cols = 0, rows = 0;
     for (coord c : coords) {
         if (c.first  > cols) { cols = c.first;  }
         if (c.second > rows) { rows = c.second; }
     }
     // Now create the bool field
-    vector<vector<bool>> out;
+    string out = "";
     for (size_t y = 0; y <= rows; y++) {
-        vector<bool> line;
         for (size_t x = 0; x <= cols; x++) {
-            line.push_back(false);
+            out += ' ';
         }
-        out.push_back(line);
+        out += '\n';
     }
-
     // Fill in the marked points
     for (coord c : coords) {
-        out.at(c.second).at(c.first) = true;
+        out[c.first + (c.second * (cols+2))] = '#';
     }
     return out;
 }
 
-string prettyField(vector<vector<bool>> field) {
-    string o = "";
-    for (vector<bool> bv : field) {
-        for (bool b : bv) {
-            if (b) { 
-                o += '#';
-            } else {
-                o += '.';
-            }
-        }
-        o += '\n';
-    }
-
-    return o;
+string task2(set<coord> *coords, vector<string> *instructions) {
+    return createPattern(instructions, *coords);
 }
 
-string task2(vector<coord> *coords, vector<string> *instructions) {
-    vector<vector<bool>> field = createPattern(*instructions, *coords);
-    return prettyField(field);
-}
-
-int task1(vector<coord> *coords, string f) {
+int task1(set<coord> *coords, string f) {
     // We only want to execute one fold, so we only send one fold. Life is simple :)
-    vector<vector<bool>> field = createPattern({ f }, *coords);
+    vector<string> fold = {f};
+    string field = createPattern(&fold, *coords);
 
     // Count the non-set points
     int count = 0;
-    for (vector<bool> bv : field) {
-        for (bool b : bv) {
-            if (b) { count++; }
+    for (char b : field) {
+        if (b == '#') {
+            count++;
         }
     }
-
     return count;
 }
 
 int main() {
-    vector<coord> coords;
+    set<coord> coords;
     vector<string> folds;
     std::ifstream is("input");
     int colums = 0, rows = 0;
@@ -120,13 +109,14 @@ int main() {
                 coord c;
                 c.first = stoi(str.substr(0, str.find_first_of(',')));
                 c.second = stoi(str.substr(str.find_first_of(',')+1));
-                coords.push_back(c);
+                coords.insert(c);
                 if (c.first > colums) { colums = c.first;  }
                 if (c.second > rows ) { rows   = c.second; }
             }
         }
     }
 
+    
     stringstream msg;
     msg << "Task 1: " << task1(&coords, folds.at(0)) << endl;
     msg << "Task 2: " << endl << task2(&coords, &folds) << endl;
