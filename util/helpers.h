74e99ae54c4699247383985da13de41956c95073
Estugon <estugon-tem@gmx.de>
2021-12-13 23:13:40 +0100

    smol helper functions will be expanded in the future

---
+++ util/helpers.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> coord;
+
+string pretty(coord c) {
+    string o = "[" + to_string(c.first) + ", " + to_string(c.second) + "]";
+    return o;
+}
+
+string pretty(vector<coord> v) {
+    string o = "";
+    for (int i = 0; i < v.size()-1; i++) {
+        o += pretty(v.at(i)) + ", ";
+    }
+    o += pretty(v.at(v.size()-1));
+    return o;
+}
+
+template <typename T>
+string pretty(vector<T> v) {
+    string o = "";
+    for (size_t i = 0; i < v.size()-1; i++) {
+        o += v.at(i) + ", ";
+    }
+    o += v.at(v.size()-1);
+    return o;
+}
+
